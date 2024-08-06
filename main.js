81538dbde2a72dfc197e42adecf89435a91d9920
TowafH <towafh@nycstudents.net>
2024-08-06 15:45:04 -0400

    Utilized forLoop to include all base_stat totals

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 //Select Elements
+
+//Page Content
 let pokeInput = document.getElementById("pokeInput");
 let pokeBtn = document.getElementById("pokeBtn");
 let pokeName = document.getElementById("pokeName")
 let pokeImg = document.getElementById("pokeImg");
+let statsTable = document.getElementById("statsTable");
+
+//Sound
 let pokeSound = null;
+
+//Stats Elements
+
+// let pokeHP = document.getElementById("pokeHP");
+// let pokeAtk = document.getElementById("pokeAtk");
+// let pokeDef = document.getElementById("pokeDef");
+// let pokeSpAtk = document.getElementById("pokeSpAtk");
+// let pokeSpDef = document.getElementById("pokeSpDef");
+// let pokeSpeed = document.getElementById("pokeSpeed");
+
+let pokeStat = document.querySelectorAll(".pokeStat")
+console.log(pokeStat)
 
 //Add Function
 function generatePokemon(event){
@@ -23,16 +40,24 @@ function generatePokemon(event){
     })
     .then(function(myPokeData){
 //View the returned response.json(); in the Console with this parameter+console.log
-        console.log(myPokeData)
+        console.log(myPokeData.stats)
 
-//h1 styles
+        //h1 data
         pokeName.innerText = myPokeData.name;
 
-//Image styles
+        //Image data
         pokeImg.src = myPokeData.sprites.front_default;
+        //Image styles
         pokeImg.style.width = "300px";
         pokeImg.style.height = "300px";
         pokeImg.style.marginTop = "-50px";
+
+        //Table data
+        statsTable.style.display = "block";
+        statsTable.style.display = "flex";
+        for (i = 0; i < pokeStat.length; i++){
+            pokeStat[i].innerText = myPokeData.stats[i].base_stat;
+        }
 
 //Function with Sound
         function makeSound(){
